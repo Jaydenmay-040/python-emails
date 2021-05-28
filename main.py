69c5551d49af7d552087c55d97cc972ceda6e589
Jaydenmay-040 <82215016+Jaydenmay-040@users.noreply.github.com>
2021-05-28 14:30:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import smtplib
-# please note go to https://myaccount.google.com/lesssecureapps and
-# turn it off to allow gmail to send
-# creates SMTP session
-s = smtplib.SMTP('smtp.gmail.com', 587)
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+
 sender_email_id = 'jaydenmay040@gmail.com'
 receiver_email_id = 'brentleejohnson73@gmail.com, jasoncee017@gmail.com, naeemahdavis@gmail.com'
-password = input("Enter senders email password")
+password = input("Enter your password")
+subject = "Greetings"
+msg = MIMEMultipart()
+msg['From'] = sender_email_id
+msg['To'] = receiver_email_id
+msg['Subject'] = subject
+body = "Hi guys:) Do yuo fnid tihs smilpe to raed?\n\n"
+body = body + "Bceuase of the phaonmneal pweor of the hmuan mnid, msot plepoe do."
+msg.attach(MIMEText(body, 'plain'))
+text = msg.as_string()
+s = smtplib.SMTP('smtp.gmail.com', 587)
 # start TLS for security
 s.starttls()
 # Authentication
 s.login(sender_email_id, password)
 # message to be sent
-message = "Hi guys\n"
-message = message + "Do yuo fnid tihs smilpe to raed?\n"
-message = message + "Bceuase of the phaonmneal pweor of the hmuan mnid, msot plepoe do."
+
 # sending the mail
-s.sendmail(sender_email_id, receiver_email_id, message)
+s.sendmail(sender_email_id, receiver_email_id, text)
 # terminating the session
 s.quit()
